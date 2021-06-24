56ff531f21c100cf905e0c249935529741a72d8f
Lucimar Neves <lucimar.neves@gmail.com>
2021-06-24 16:59:29 -0300

    Estrutura de componentes

---
+++ src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+import { AuthContextProvider } from './contexts/AuthContext';
+
+
+
+
+
+function App() {
+
+  return (
+    <AuthContextProvider>
+      <BrowserRouter>
+        <Route path="/" exact={true} component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </BrowserRouter>
+    </AuthContextProvider>
+  )
+}
+
+export default App;
